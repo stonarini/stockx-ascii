556060b0e5bbc4fac31021a26806cf2e29124acb
stonarini <stonarini@cifpfbmoll.eu>
2022-02-17 13:02:07 +0100

    chore: add second user story

--- src/main/java/edu/poniperro/Stockx.java
+++ src/main/java/edu/poniperro/Stockx.java
@@ -2,6 +2,7 @@ package edu.poniperro;
 
 import edu.poniperro.item.Item;
 import edu.poniperro.item.Sneaker;
+import edu.poniperro.offer.Bid;
 
 /**
  * StockX nació en Detroit, y allí siguen haciendo
@@ -42,12 +43,12 @@ public class Stockx {
          * en sus offers.
          */
 
-        // Bid bid = new Bid("13", 550);
-        // sneaker.add(bid);
-        // sneaker.add(new Bid("6", 200));
-        // sneaker.add(new Bid("9.5", 479));
-        // sneaker.add(new Bid("13", 338));
-        // sneaker.add(new Bid("9.5", 480));
+        Bid bid = new Bid("13", 550);
+        sneaker.add(bid);
+        sneaker.add(new Bid("6", 200));
+        sneaker.add(new Bid("9.5", 479));
+        sneaker.add(new Bid("13", 338));
+        sneaker.add(new Bid("9.5", 480));
 
         /**
          * Crear asks
